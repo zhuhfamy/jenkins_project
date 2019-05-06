a294483a5fef9f741998cac0653ed68cfbfb11c5
zhuhfamy <zhuhf_amy@163.com>
2019-05-06 11:48:14 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+class TestLogin:
+
+    def test_login1(self):
+        print("test_login1")
+        assert 1
+
+    def test_login2(self):
+        print("test_login2")
+        assert 0
+
+    def test_login3(self):
+        print("test_login3")
+        assert 1
